de171c5ce3d0ef1bc300c80f6f11bee67f846e46
Mattis <matjen@mail.de>
2025-03-25 23:37:34 +0100

    Initial Booking impl

---
+++ domain/dk.sdu.mmmi.pms.domain.booking/src/main/java/dk/sdu/mmmi/pms/domain/booking/Booking.java
@@ -0,0 +1,34 @@
+package dk.sdu.mmmi.pms.domain.booking;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class Booking {
+    private final UUID id;
+    private final LocalDateTime startTime;
+    private final LocalDateTime endTime;
+    private final UUID bookedParkingSpotId;
+    private final UUID bookedByUserID;
+
+    public Booking(UUID id, LocalDateTime startTime, LocalDateTime endTime, UUID bookedParkingSpotId, UUID bookedByUserID) {
+        validateBookingTime(startTime, endTime);
+        this.id = id;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.bookedParkingSpotId = bookedParkingSpotId;
+        this.bookedByUserID = bookedByUserID;
+    }
+
+    private void validateBookingTime(LocalDateTime start, LocalDateTime end) {
+        if (end.isBefore(start)) {
+            throw new IllegalArgumentException("End time must be after start time.");
+        }
+    }
+
+    // Getters
+    public UUID getId() { return id; }
+    public LocalDateTime getStartTime() { return startTime; }
+    public LocalDateTime getEndTime() { return endTime; }
+    public UUID getBookedParkingSpotId() { return bookedParkingSpotId; }
+    public UUID getBookedByUserID() { return bookedByUserID; }
+}
