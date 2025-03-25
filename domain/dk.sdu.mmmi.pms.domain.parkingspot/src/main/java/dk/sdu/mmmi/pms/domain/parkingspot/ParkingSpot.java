d1bd8ea624fc6695ea7468121484f5bb531ccb5c
Mattis <matjen@mail.de>
2025-03-25 23:38:10 +0100

    Initial ParkingSpot impl

---
+++ domain/dk.sdu.mmmi.pms.domain.parkingspot/src/main/java/dk/sdu/mmmi/pms/domain/parkingspot/ParkingSpot.java
@@ -0,0 +1,27 @@
+package dk.sdu.mmmi.pms.domain.parkingspot;
+
+import java.util.UUID;
+
+public class ParkingSpot {
+    private final UUID id;
+    private final int location;
+    private final UUID belongsToParkingLotId;
+
+    public ParkingSpot(UUID id, int location, UUID belongsToParkingLotId) {
+        validateLocation(location);
+        this.id = id;
+        this.location = location;
+        this.belongsToParkingLotId = belongsToParkingLotId;
+    }
+
+    private void validateLocation(int location) {
+        if (location < 0) {
+            throw new IllegalArgumentException("Invalid location");
+        }
+    }
+
+    // Getters
+    public UUID getId() { return id; }
+    public int getLocation() { return location; }
+    public UUID getBelongsToParkingLotId() { return belongsToParkingLotId; }
+}
--- domain/dk.sdu.mmmi.pms.domain.parkingspot/src/main/java/module-info.java
+++ domain/dk.sdu.mmmi.pms.domain.parkingspot/src/main/java/module-info.java
@@ -1,5 +1,2 @@
 module dk.sdu.mmmi.pms.domain.parkingspot {
-//    requires modulename;
-//    opens dk.sdu.mmmi.cbse.domain.packagename;
-//    exports dk.sdu.mmmi.cbse.domain.packagename;
 }
