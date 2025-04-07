dfa4f262a9caff9060f41d60ce7a5f69fdc2144f
Mattis <matjen@mail.de>
2025-04-07 14:51:41 +0200

    Create Account Controller

--- presentation/dk.sdu.mmmi.pms.presentation.account/src/main/java/dk/sdu/mmmi/pms/presentation/account/AccountController.java
+++ presentation/dk.sdu.mmmi.pms.presentation.account/src/main/java/dk/sdu/mmmi/pms/presentation/account/AccountController.java
@@ -1,38 +1,29 @@
 package dk.sdu.mmmi.pms.presentation.account;
 
 import dk.sdu.mmmi.pms.application.account.CreateAccountUseCase;
-import dk.sdu.mmmi.pms.core.account.Account;
-import dk.sdu.mmmi.pms.core.account.AccountRole;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/accounts")
+@RequestMapping("/api/account")
 public class AccountController {
-//    private final CreateAccountUseCase createAccountUseCase;
-//
-//    public AccountController(CreateAccountUseCase createAccountUseCase) {
-//        this.createAccountUseCase = createAccountUseCase;
-//    }
+    private final CreateAccountUseCase createAccountUseCase;
 
-    public AccountController() {}
+    public AccountController(CreateAccountUseCase createAccountUseCase) {
+        this.createAccountUseCase = createAccountUseCase;
+    }
 
-//    @PostMapping
-//    @ResponseStatus(HttpStatus.CREATED)
-//    public AccountResponse createAccount(@RequestBody CreateAccountRequest request) {
-//        UUID accountId = createAccountUseCase.execute(
-//                request.name(),
-//                request.email(),
-//                request.password(),
-//                request.role()
-//        );
-//        return new AccountResponse(accountId, request.name(), request.email(), request.role());
-//    }
-
-    @GetMapping
-    public String getAccount() {
-        return "Hello, this is the account endpoint!";
+    @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public AccountResponse createAccount(@RequestBody CreateAccountRequest request) {
+        UUID accountId = createAccountUseCase.execute(
+                request.name(),
+                request.email(),
+                request.password(),
+                request.role()
+        );
+        return new AccountResponse(accountId, request.name(), request.email(), request.role());
     }
 }
