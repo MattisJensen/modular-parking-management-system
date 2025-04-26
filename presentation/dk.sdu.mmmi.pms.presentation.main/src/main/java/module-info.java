1e6b8e011a4f9ac67235585e35a823ab56c7af29
Mattis <matjen@mail.de>
2025-04-27 01:40:38 +0200

    Dependency cleanup

--- presentation/dk.sdu.mmmi.pms.presentation.main/src/main/java/module-info.java
+++ presentation/dk.sdu.mmmi.pms.presentation.main/src/main/java/module-info.java
@@ -1,6 +1,10 @@
 import dk.sdu.mmmi.pms.application.shared.ModuleConfigurationSPI;
 
 module dk.sdu.mmmi.pms.presentation.main {
+    // Internal dependencies
+    requires dk.sdu.mmmi.pms.application.shared;
+    uses ModuleConfigurationSPI;
+
     // External dependencies
     requires spring.context;
     requires spring.web;
@@ -9,12 +13,6 @@ module dk.sdu.mmmi.pms.presentation.main {
     requires org.apache.tomcat.embed.core;
     requires jakarta.annotation;
 
-    opens dk.sdu.mmmi.pms.presentation.main to spring.beans, spring.core, spring.context;
+    opens dk.sdu.mmmi.pms.presentation.main to spring.beans, spring.context, spring.core;
     opens dk.sdu.mmmi.pms.presentation.main.config to spring.beans, spring.context, spring.core;
-    opens dk.sdu.mmmi.pms.presentation.main.spring to spring.beans, spring.context, spring.core;
-    opens dk.sdu.mmmi.pms.presentation.main.tomcat to spring.beans, spring.context, spring.core;
-
-    // Internal dependencies
-    requires dk.sdu.mmmi.pms.application.shared;
-    uses ModuleConfigurationSPI;
 }
