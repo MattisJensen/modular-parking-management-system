0e2165478c5e4c91ded86bdbd8d6263f19ce44de
Mattis <matjen@mail.de>
2025-05-06 17:06:24 +0200

    spot repo impl.

---
+++ infrastructure/dk.sdu.mmmi.pms.infrastructure.parkingspot/src/main/java/dk/sdu/mmmi/pms/infrastructure/parkingspot/persistence/ParkingSpotMapper.java
@@ -0,0 +1,25 @@
+package dk.sdu.mmmi.pms.infrastructure.parkingspot.persistence;
+
+import dk.sdu.mmmi.pms.core.parkingspot.ParkingSpot;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ParkingSpotMapper {
+    public ParkingSpotJpaEntity toJpaEntity(ParkingSpot core) {
+        return new ParkingSpotJpaEntity(
+                core.id(),
+                core.parkingLotId(),
+                core.spotIdentifier(),
+                core.status()
+        );
+    }
+
+    public ParkingSpot toCore(ParkingSpotJpaEntity jpa) {
+        return new ParkingSpot(
+                jpa.getId(),
+                jpa.getParkingLotId(),
+                jpa.getSpotIdentifier(),
+                jpa.getStatus()
+        );
+    }
+}
