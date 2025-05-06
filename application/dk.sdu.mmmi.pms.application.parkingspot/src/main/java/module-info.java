19ee91f06aaa7ddacd5e4ecbd1b2eb2bdcf26db1
Mattis <matjen@mail.de>
2025-05-06 17:05:38 +0200

    spot repo interface

--- application/dk.sdu.mmmi.pms.application.parkingspot/src/main/java/module-info.java
+++ application/dk.sdu.mmmi.pms.application.parkingspot/src/main/java/module-info.java
@@ -1,2 +1,10 @@
 module dk.sdu.mmmi.pms.application.parkingspot {
+    // Internal dependencies
+    requires dk.sdu.mmmi.pms.application.parkinglot;
+    requires dk.sdu.mmmi.pms.core.parkingspot;
+    requires dk.sdu.mmmi.pms.core.parkinglot;
+
+    // Visibility
+    exports dk.sdu.mmmi.pms.application.parkingspot;
+    exports dk.sdu.mmmi.pms.application.parkingspot.usecase;
 }
