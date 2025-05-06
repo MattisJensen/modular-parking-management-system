24c14bc6b85079004e4ce427d16f08ad3e6d32fd
Mattis <matjen@mail.de>
2025-05-06 17:05:38 +0200

    spot repo interface

---
+++ application/dk.sdu.mmmi.pms.application.parkingspot/src/main/java/dk/sdu/mmmi/pms/application/parkingspot/ParkingSpotRepository.java
@@ -0,0 +1,16 @@
+package dk.sdu.mmmi.pms.application.parkingspot;
+
+import dk.sdu.mmmi.pms.core.parkingspot.ParkingSpot;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ParkingSpotRepository {
+    void save(ParkingSpot parkingSpot);
+    void update(ParkingSpot parkingSpot);
+    void deleteById(UUID id);
+    Optional<ParkingSpot> findById(UUID id);
+    List<ParkingSpot> findByParkingLotId(UUID parkingLotId);
+    List<ParkingSpot> findAvailableSpotsByParkingLotId(UUID parkingLotId);
+}
